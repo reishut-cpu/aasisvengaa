dfb747cd83c8d8191f60af1200874ce66657d153
Kimi Pahkala <freegotfreefree@gmail.com>
2024-11-24 19:30:27 +0200

    Version 12

---
+++ pitches/pitches.c
@@ -0,0 +1,6 @@
+// Code referenced from https://github.com/hibit-dev/buzzer/tree/master/lib
+// -----------------------------------------------------
+// Copyright (c) 2021-2023 HiBit <https://www.hibit.dev>
+// -----------------------------------------------------
+
+#include "pitches.h"
--- project_main.c
+++ project_main.c
@@ -1,3 +1,8 @@
+// Every one of us (Kimi Pahkala, Petrus Ollikainen and Henri Seppälä) were responsible
+// for a little bit of everything in the code. We unfortunately cannot exactly pin-point
+// which code was written by whom, as we always coded in a group. We can presumably say that
+// that everything we did here was with utmost care by us. :')
+
 /* C Standard library */
 #include <sensors/buzzer.h>
 #include <stdio.h>
@@ -25,7 +30,26 @@
 #include "sensors/opt3001.h"
 #include "sensors/buzzer.h"
 
-/* Board */
+/* Music library, referenced files from */
+/* https://projecthub.arduino.cc/tmekinyan/playing-popular-songs-with-arduino-and-a-buzzer-546f4a */
+#include "pitches/pitches.h"
+
+/* Music notes to apply to play Nokia theme song */
+/* Referenced from https://github.com/hibit-dev/buzzer/blob/master/src/other/nokia/nokia.ino */
+
+int melody[] = {
+  NOTE_E5, NOTE_D5, NOTE_FS4, NOTE_GS4,
+  NOTE_CS5, NOTE_B4, NOTE_D4, NOTE_E4,
+  NOTE_B4, NOTE_A4, NOTE_CS4, NOTE_E4,
+  NOTE_A4
+};
+
+int durations[] = {
+  8, 8, 4, 4,
+  8, 8, 4, 4,
+  8, 8, 4, 4,
+  2
+};
 
 /* Task */
 #define STACKSIZE 2048
@@ -42,12 +66,14 @@ PIN_Config cBuzzer[] = {
 
 // Define global variables for UART task
 int UART_ID = 0;
+int SpaceCounter = 0;
 
-// JTKJ: Teht�v� 3. Tilakoneen esittely
-// JTKJ: Exercise 3. Definition of the state machine
-enum state { WAITING=1, DATA_READY };
+// State Machine
+enum state { WAITING=1, DATA_READY};
 enum state programState = WAITING;
 
+// Struct for giving an additional name for readability for
+// MPU9250 sensors
 typedef struct{
     float ax;
     float ay;
@@ -58,8 +84,11 @@ typedef struct{
 } SensorData;
 SensorData sensor; // Määritellään muuttuja gyro yhteiseksi
 
+//Prototypes
 void mpu9250_get_data(I2C_Handle *i2c, float *ax, float *ay, float *az, float *gx, float *gy, float *gz);
 void mpu9250_setup(I2C_Handle *i2c);
+void musicLoop();
+void playBuzzer(int freq, int time_buzzer, int time_sleep);
 
 // MPU power pin global variables
 static PIN_Handle hMpuPin;
@@ -86,8 +115,6 @@ static const I2CCC26XX_I2CPinCfg i2cMPUCfg = {
 
 void buttonFxn(PIN_Handle handle, PIN_Id pinId) {
 
-    // JTKJ: Teht�v� 1. Vilkuta jompaa kumpaa ledi�
-    // JTKJ: Exercise 1. Blink either led of the device
     // Vaihdetaan led-pinnin tilaa negaatiolla
        /*uint_t pinValue = PIN_getOutputValue( Board_LED0 );
        pinValue = !pinValue;
@@ -96,9 +123,6 @@ void buttonFxn(PIN_Handle handle, PIN_Id pinId) {
 
 /* Task Functions */
 Void uartTaskFxn(UArg arg0, UArg arg1) {
-
-    // JTKJ: Teht�v� 4. Lis�� UARTin alustus: 9600,8n1
-    // JTKJ: Exercise 4. Setup here UART connection as 9600,8n1
 
       UART_Handle uart;
       UART_Params uartParams;
@@ -125,24 +149,26 @@ Void uartTaskFxn(UArg arg0, UArg arg1) {
                    case 1: // ID for x-axis
                        UART_write(uart, ".\r\n\0", 4);
                        UART_ID = 0;
+                       SpaceCounter = 0;
                        break;
                    case 2: // ID for Y-axis
                        UART_write(uart, "-\r\n\0", 4);
                        UART_ID = 0;
+                       SpaceCounter = 0;
                        break;
                    case 3: // ID for Z-axis
                        UART_write(uart, " \r\n\0", 4);
                        UART_ID = 0;
+                       if (SpaceCounter == -1) {
+                           musicLoop();
+                       }
+                       SpaceCounter++;
                        break;
                    }
                    programState = WAITING;
                }
 
-        /*System_printf("uartTask\n");
-        System_flush();*/
-
-        // Once per second, you can modify this
-        Task_sleep(100000 / Clock_tickPeriod);
+        Task_sleep(100000 / Clock_tickPeriod); // 100ms
     }
 }
 
@@ -156,15 +182,48 @@ Void buzzerTaskFxn(UArg arg0, UArg arg1) {
 
         Task_sleep(950000 / Clock_tickPeriod);
       }
-
 }
+
+// Function that is responsible for a simple buzz
+void playBuzzer(int freq, int time_buzzer, int time_sleep) {
+    buzzerOpen(hBuzzer);
+    buzzerSetFrequency(freq); //Frequency to be played on SensorTag's buzzer
+    Task_sleep(time_buzzer / Clock_tickPeriod); // Amount of time that buzzer plays for (e.g. 80000 = 80ms)
+    buzzerClose();
+    Task_sleep(time_sleep / Clock_tickPeriod); // Amount of time that this task sleeps after playing buzz (e.g. 400000 = 400ms)
+}
+
+// Function, that utilizes a "for loop" to play a tune through the buzzer
+Void musicLoop() {
+  int size = sizeof(durations) / sizeof(durations[0]);
+  int note;
+  for (note = 0; note < size; note++) {
+    //Open Buzzer
+    buzzerOpen(hBuzzer);
+
+    //Input the specific frequency to be played
+    buzzerSetFrequency(melody[note]);
+
+    //This sleep is responsible for the duration of the tone to be palyed
+    int duration = ((1000 / durations[note]) * 1000);
+    Task_sleep(duration / Clock_tickPeriod);
+
+    // Stop the specific tone from playing
+    buzzerClose();
+
+    //To seperate the tones from playing one after another, set a delay
+    //After playing tone. In this case, 130% of tone duration
+    int pauseBetweenNotes = duration * 1.30;
+    Task_sleep(pauseBetweenNotes / Clock_tickPeriod);
+  }
+}
+
 Void sensorTaskFxn(UArg arg0, UArg arg1) {
 
     I2C_Handle      i2cMPU;
     I2C_Params      i2cMPUParams;
 
-    // JTKJ: Teht�v� 2. Avaa i2c-v�yl� taskin k�ytt��n
-    // JTKJ: Exercise 2. Open the i2c bus
+    // Open I2C bus
     I2C_Params_init(&i2cMPUParams);
     i2cMPUParams.bitRate = I2C_400kHz;
     i2cMPUParams.custom = (uintptr_t)&i2cMPUCfg;
@@ -195,62 +254,45 @@ Void sensorTaskFxn(UArg arg0, UArg arg1) {
         Task_sleep(10000 / Clock_tickPeriod);
 
         while (1) {
-            // Function below is responsible for reading the data from sensor data
             mpu9250_get_data(&i2cMPU, &sensor.ax, &sensor.ay, &sensor.az, &sensor.gx, &sensor.gy, &sensor.gz);
             // Keep reading data, until one of the determined sensors exceed the threshold
             if (programState == WAITING) {
-            // Below are required thresholds for different movenents on set axises. They are responsible for setting different ID's for UART to print
-            if (sensor.ax > 0.5) { // When X-axis exceeds this threshold, goes to the below functions
-                //System_printf(".\r\n\0");
-                UART_ID = 1; // Set identifier for UART Task
-                System_flush();
-                buzzerOpen(hBuzzer);
-                buzzerSetFrequency(2000);
-                Task_sleep(150000 / Clock_tickPeriod); // 150ms
-                buzzerClose();
-                Task_sleep(250000 / Clock_tickPeriod); // 250ms
-                programState = DATA_READY;
 
-            } else if (sensor.ay > 0.5) { // When Y-axis exceeds this threshold, goes to the below functions
-                //System_printf("-\r\n\0");
-                UART_ID = 2; // Set identifier for UART Task
-                System_flush();
-                buzzerOpen(hBuzzer);
-                buzzerSetFrequency(2000);
-                Task_sleep(500000 / Clock_tickPeriod); // 500ms
-                buzzerClose();
-                programState = DATA_READY;
+                // Below are required thresholds for different movenents on set axises.
+                // They are responsible for setting different ID's for UART to print
 
-            } else if (sensor.az > -0.5) { // When Z-axis exceeds this threshold, goes to the below functions
-                //System_printf("VELI\r\n\0");
-                UART_ID = 3; // Set identifier for UART Task
-                System_flush();
-                buzzerOpen(hBuzzer);
-                buzzerSetFrequency(2000);
-                Task_sleep(20000 / Clock_tickPeriod); // 20ms
-                buzzerClose();
-                Task_sleep(280000 / Clock_tickPeriod); // 280ms
-                programState = DATA_READY;
+                if (sensor.ax > 0.5) { // When X-axis acc exceeds this threshold, goes to the below functions
+                    UART_ID = 1; // Set identifier for UART Task
+                    playBuzzer(2000, 150000, 250000);
+                    programState = DATA_READY;
 
-            // Below are thresholds for handling error data. For example, the SensorTag is in a wrong position according to acceleration
-            } else if (sensor.ax > 1.0) {
-                Task_sleep(100000 / Clock_tickPeriod); // 100ms
+                } else if (sensor.ay > 0.5) { // When Y-axis acc exceeds this threshold, goes to the below functions
+                    UART_ID = 2; // Set identifier for UART Task
+                    playBuzzer (2000, 400000, 100000);
+                    programState = DATA_READY;
 
-            } else if (sensor.ay > 1.0) {
-                Task_sleep(100000 / Clock_tickPeriod); // 100ms
+                } else if (sensor.az > -0.5) { // When Z-axis acc exceeds this threshold, goes to the below functions
+                    UART_ID = 3; // Set identifier for UART Task
+                    if (SpaceCounter == 2) {
+                        playBuzzer(3000, 50000, 250000); // Play thrice to indicate three spaces have been written
+                        playBuzzer(3000, 50000, 250000);
+                        playBuzzer(3000, 50000, 250000);
+                        SpaceCounter = -1; // Indication to play music in UartTask
+                    }
+                    else if (SpaceCounter == 1) {
+                        playBuzzer(3000, 50000, 250000);
+                        playBuzzer(3000, 50000, 250000); // Play twice to indicate two spaces have been written
+                    } else {
+                        playBuzzer(3000, 50000, 250000); // Play once to indicate one space have been written
+                    }
+                    programState = DATA_READY;
 
-            } else if (sensor.az > 1.0) {
-                Task_sleep(100000 / Clock_tickPeriod); // 100ms
+                // Below are thresholds for handling error data. For example, the SensorTag is in a wrong position according to acceleration
+                } else { // Default sleep, when no matching criteria are met
+                    Task_sleep(100000 / Clock_tickPeriod); // 100ms
+                }
 
-            } else { // Default sleep, when no matching criteria are met
-                Task_sleep(100000 / Clock_tickPeriod); // 100ms
-            }
-
-
-            }
-            // Once per second, you can modify this
-            Task_sleep(50000 / Clock_tickPeriod);
-
+                }
     }
 }
 
@@ -266,29 +308,8 @@ Int main(void) {
     Board_initGeneral();
     Board_initI2C();
     
-    // JTKJ: Teht�v� 4. Ota UART k�ytt��n ohjelmassa
-    // JTKJ: Exercise 4. Initialize UART
+    // Initialize UART
     Board_initUART();
-    // JTKJ: Teht�v� 1. Ota painonappi ja ledi ohjelman k�ytt��n
-    //       Muista rekister�id� keskeytyksen k�sittelij� painonapille
-    // JTKJ: Exercise 1. Open the button and led pins
-    //       Remember to register the above interrupt handler for button
-    // Ledi käyttöön ohjelmassa
-    /*ledHandle = PIN_open( &ledState, ledConfig );
-    if(!ledHandle) {
-       System_abort("Error initializing LED pin\n");
-    }*/
-
-    // Painonappi käyttöön ohjelmassa
-    /*buttonHandle = PIN_open(&buttonState, buttonConfig);
-    if(!buttonHandle) {
-       System_abort("Error initializing button pin\n");
-    }*/
-
-    // Painonapille keskeytyksen käsittellijä
-    /*if (PIN_registerIntCb(buttonHandle, &buttonFxn) != 0) {
-       System_abort("Error registering button callback function");
-    }*/
 
     // Open MPU power pin
     hMpuPin = PIN_open(&MpuPinState, MpuPinConfig);
